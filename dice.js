e9f9051de9e324a1491ecb7ef4b2837474b21102
Anuj Tuteja <anujtuteja@Anujs-MacBook-Pro.local>
2022-07-30 02:10:16 +1200

    Cleaning up the code

--- dice.js
+++ dice.js
@@ -1,13 +1,12 @@
-function generateRandomIntegerInRange(min, max) {
+function generateRandomIntegerInRange(min, max)                 //function to generate a random number between min and max values
+{
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function dice()
+function dice()                                                 //function to get the dice type and update the HTML with the generated value
 {   
     let Value = document.getElementById("dice");
     let side = Value.options[Value.selectedIndex].value;
     let result = generateRandomIntegerInRange(1,side);
     document.getElementById("roll").innerHTML = result;
-    //element = document.getElementsByClassName("result");
-    //element.classList.add("transform");
 }
